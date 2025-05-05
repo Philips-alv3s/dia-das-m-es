be2a8c40b28bc77ed5c0780e1a4ac77bec22ff6e
Philips-alv3s <00001112732329sp@al.educacao.sp.gov.br>
2025-05-05 13:08:05 -0300

    Primeiro commit

---
+++ teste.py
@@ -0,0 +1,22 @@
+import time
+mensagem = [
+    "Feliz dias das Mães!",
+      "Mãe, Obrigado por tudo voce é muito especial na minha vida, sempre fez tudo por mim e eu sou muito grato por tudo, prometo dedicar todo meu esforço na escola,no curso e se Deus quiser num futuro trabalho, meu motivo de querer continuar vivendo e lutar dia a dia querendo conseguir sendo uma pessoa melhor e que possa te dar orgulho, quero te mostrar que tudo que voce fez por mim não foi em vão",
+      "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+      "Saiba que todo meu esforço eu faço por voce, Querendo te ver alegre ",
+      "Te amo muito!!",
+      "Beijos,te amo"]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(11.0)
+
+    print()
+    print("    **     **")
+    print("  ****** *****")
+    print(" **************")
+    print("  ************")
+    print("   **********")
+    print("     ******")
+    print("      ****")
+    print("       **")
